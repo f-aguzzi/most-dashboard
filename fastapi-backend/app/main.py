afac1ffcb599836020544df9e48329e78942487d
f-aguzzi <62149513+f-aguzzi@users.noreply.github.com>
2025-09-11 11:15:54 +0200

    fix: backend CORS

--- fastapi-backend/app/main.py
+++ fastapi-backend/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 import polars as pl
 from app.service import get_all_flights, filter_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 data = pl.read_excel("data.xlsx")
 
 @app.get("/airports")
