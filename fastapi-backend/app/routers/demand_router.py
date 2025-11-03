b2a37c481ea9ea0a8e20a6d5b8c402b8d1782759
f-aguzzi <62149513+f-aguzzi@users.noreply.github.com>
2025-11-03 12:01:05 +0100

    feat: demand router

--- fastapi-backend/app/routers/demand_router.py
+++ fastapi-backend/app/routers/demand_router.py
@@ -4,7 +4,23 @@ from fastapi import APIRouter
 demand_router = APIRouter()
 
 # Caricamento dati
-passenger_monthly = pl.LazyFrame(pl.read_excel("air_passenger_monthly_2035.xlsx"))
-passenger_yearly = pl.LazyFrame(pl.read_excel("air_passenger_yearly_2035.xlsx"))
-freight_montly = pl.LazyFrame(pl.read_excel("freight_monthly_data 2035.xlsx"))
-freight_yearly = pl.LazyFrame(pl.read_excel("freight_yearly 2035.xlsx"))
+passenger_monthly = pl.read_excel("air_passenger_monthly_2035.xlsx")
+passenger_yearly = pl.read_excel("air_passenger_yearly_2035.xlsx")
+freight_monthly = pl.read_excel("freight_monthly_data 2035.xlsx")
+freight_yearly = pl.read_excel("freight_yearly 2035.xlsx")
+
+
+@demand_router.get("/passenger")
+def get_passenger(timeframe: str):
+    if timeframe == "monthly":
+        return passenger_monthly.to_dicts()
+    else:
+        return passenger_yearly.to_dicts()
+
+
+@demand_router.get("/freight")
+def get_freight(timeframe: str):
+    if timeframe == "monthly":
+        return freight_monthly.to_dicts()
+    else:
+        return freight_yearly.to_dicts()
